be6d55f2580fa2a7ec3fbc9423ae69bc7f3a0fc6
AllAroundD <AllAroundDWeb@gmail.com>
2020-06-04 11:44:08 -0400

    made initial movie api query and notes

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
 console.log('hi')
 let foodApi = 'https://food-by-mood.herokuapp.com/api/foods'
-// let movieApi = 
+let movieApi = 'https://api.themoviedb.org/3/genre/movie/list?api_key=dbd68826ec7649f3671ac738ca17fe12&language=en-US' //check documentation
+let moods = [
+    "Neutral",
+    "Relaxed",
+    "Energetic"
+]
+
 
 async function fetchData(api){
 
@@ -9,7 +15,7 @@ async function fetchData(api){
     return result
 }
 
-async function test(){
+async function FoodData(){
     foodObj = await fetchData(foodApi)
     // movieObj = fetchData(movieApi)
     console.log(foodObj)
@@ -17,4 +23,22 @@ async function test(){
     contentEl.innerHTML = foodObj[0].title
 }
 
-test()
+async function MovieData(){
+    movieObj = await fetchData(movieApi)
+    console.log(movieObj)
+    contentEl = document.querySelector('#content')
+    let genreNames = movieObj.name;
+    console.log(`genreNames: ${genreNames}`);
+    contentEl.innerHTML = JSON.stringify(movieObj);
+}
+
+// FoodData();
+MovieData();
+
+/*
+Notes for us to do
+array of genreNames
+match up genrenames to the 3 moods
+use genreNames to get the ids and then fetch based on genre
+*
+
